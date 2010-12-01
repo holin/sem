bb8632ea5f9a965c94774501c538970253f5a54b
HoLin <holin.he@gmail.com>
2010-12-01 12:51:11 +0800

    paginate accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,7 @@ class AccountsController < ApplicationController
   # GET /accounts
   # GET /accounts.xml
   def index
-    @accounts = Account.all
+    @accounts = Account.paginate(:page => params[:page], :per_page => 20, :order => "id desc")
 
     respond_to do |format|
       format.html # index.html.erb
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def paginate(objects)
+    if objects
+      will_paginate objects
+    end
+  end
 end
