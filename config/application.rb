619a04b3929dd6e500423b7519461507785e4dc2
HoLin <holin.he@gmail.com>
2010-12-22 10:41:29 +0800

    http basic auth

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,10 @@ module Sem
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    
+    config.middleware.use Rack::Auth::Basic do |username, password|
+      password == "mokaka"
+    end
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
